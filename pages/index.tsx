270e54a5bb56b4cf1e303ea59c6c1bd270389a74
Shirajuki <shirajuki00@gmail.com>
2021-12-19 01:14:50 +0100

    feat: adds section3 and google map with placeholder images

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ import Header from '../components/header';
 import Footer from '../components/footer';
 
 const Home: NextPage = () => {
+  const images = Array(8).fill('/images/placeholder.png');
   return (
     <div className={styles.container}>
       <Head>
@@ -161,10 +162,40 @@ const Home: NextPage = () => {
             </div>
           </div>
         </div>
-        <div className={styles.section}>
+        <div className={`${styles.section} ${styles.galleryWrapper}`}>
           <h1 className={styles.title}>Følg oss på instagram!</h1>
+          <div className={styles.gallery}>
+            {images.map((imgSrc: string, i: number) => {
+              return (
+                <div className={styles.image} key={i}>
+                  <Image
+                    src={imgSrc}
+                    alt="A placeholder for images"
+                    width={320}
+                    height={320}
+                    layout="responsive"
+                  />
+                </div>
+              );
+            })}
+          </div>
+          <svg
+            className={styles.galleryWave}
+            viewBox="0 0 1440 144"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M1440 63.7961L1359.49 73.8216C1280.18 83.8471 1119.15 103.898 959.333 87.1889C799.512 70.4798 638.488 17.0105 478.667 3.64316C318.845 -9.72416 157.822 17.0105 78.5116 30.3778L-2 43.7451V144H78.5116C157.822 144 318.845 144 478.667 144C638.488 144 799.512 144 959.333 144C1119.15 144 1280.18 144 1359.49 144H1440V63.7961Z"
+              fill="#F4EEA9"
+            />
+          </svg>
         </div>
-        <div className={`${styles.section} ${styles.map}`}>map</div>
+        <div className={`${styles.section} ${styles.map}`}>
+          <div className={styles.googleMap}></div>
+        </div>
       </main>
 
       <Footer />
