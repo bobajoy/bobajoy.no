f345c80ccf0eef94a1d96d18d417539f2add4ba5
Shirajuki <shirajuki00@gmail.com>
2021-12-30 19:56:44 +0100

    chore: fixes google font preconnect

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,11 +12,7 @@ class MyDocument extends Document<DocumentProps> {
       <Html lang="no">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin=""
-          />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:wght@400;500;600;700;800&family=Coiny&family=Fuzzy+Bubbles:wght@400;700&family=Pacifico&display=swap"
             rel="stylesheet"
