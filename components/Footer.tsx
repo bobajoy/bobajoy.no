be0080f211ccde1779cee7f3fb645b4682ab5725
Shirajuki <shirajuki00@gmail.com>
2022-12-09 20:29:34 +0100

    feat: add restaurant guru widget on footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -120,6 +120,73 @@ const Footer = () => {
             </address>
           </a>
         </div>
+        <link href="https://awards.infcdn.net/circle_v2.css" rel="stylesheet" />
+        <div id="circle_v2">
+          <a href="https://restaurantguru.com/Boba-Joy-Trondheim">
+            <div className="arc-heading ">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                width="160px"
+                height="160px"
+                viewBox="0 0 160 160"
+              >
+                <defs>
+                  <path id="heading-arc" d="M 30 80 a 50 50 0 1 1 100 0"></path>
+                </defs>
+                <text
+                  className="arc-heading__heading "
+                  fill="#000"
+                  textAnchor="middle"
+                >
+                  <textPath startOffset="50%" xlinkHref="#heading-arc">
+                    Recommended
+                  </textPath>
+                </text>
+              </svg>
+            </div>
+            <div className="sq_year">2022</div>
+            <a
+              href="https://restaurantguru.com/Boba-Joy-Trondheim"
+              className="sq_center "
+              target="_blank"
+              rel="noreferrer"
+            >
+              Boba Joy
+            </a>
+            <div className="arc-heading arc-heading__bottom">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                width="120px"
+                height="120px"
+                viewBox="0 0 120 120"
+              >
+                <defs>
+                  <path
+                    id="subheading-arc"
+                    d="M 12 60 a 48 48 0 0 0 96 0"
+                  ></path>
+                </defs>
+                <text
+                  className="arc-heading__subheading"
+                  fill="#000"
+                  textAnchor="middle"
+                >
+                  <textPath startOffset="50%" xlinkHref="#subheading-arc">
+                    <a
+                      href="https://restaurantguru.com"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Restaurant Guru
+                    </a>
+                  </textPath>
+                </text>
+              </svg>
+            </div>
+          </a>
+        </div>
       </div>
       <div className="footerCreditsWrapper">
         <p>
