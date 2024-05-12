4bcceb2cb1c3f667768bf853c56e2622df0c9863
bobajoytrondheim <bobajoytrondheim@gmail.com>
2024-05-12 21:31:58 +0200

    meny

--- pages/index.tsx
+++ pages/index.tsx
@@ -96,7 +96,7 @@ const Home: NextPage = () => {
               <div>
                 <h2>Vi ønsker deg velkommen til Bobajoy</h2>
                 <p>
-                  er en ny butikk som kom til Trondheim i starten av 2022. Vi
+                  Vi er en ny butikk som kom til Trondheim i starten av 2022. Vi
                   selger bubble tea, fresh iced tea, smoothies, slushes, varme
                   drikker for vinteren, og all slags type asiatiske godsaker. Vi
                   ønsker å bli et komfortabelt og trygt sted for deg...
