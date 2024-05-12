a43aa962a08bc61cd8b9f21a5988c930074a12cf
Jonny Ngo Luong <shirajuki00@gmail.com>
2024-05-12 21:37:10 +0200

    Update post

--- pages/api/posts.ts
+++ pages/api/posts.ts
@@ -2,10 +2,20 @@
 export default function handler(_req: any, res: any) {
   res.status(200).json([
     {
-      img: 'https://lh3.googleusercontent.com/geougc/AF1QipP0hd2N0qp2aSq3frIYVUfDTRD3bLvnzJgFYTG6=h305-no',
+      img: 'https://lh3.googleusercontent.com/geougc/AF1QipOh91_EkMgrl3MAISoKhWUFRioOG35j74Ekn6ve=h305-no',
       date: new Date('2022-03-01T11:30:32.601Z'),
-      content:
-        `Velkommen til Midtbyen, Boba Joy Trondheim !  Boble-te utvalget i Midtbyen vokser seg stadig større, og for kun to uker siden åpnet denne sjarmerende Kaféen i Olav Tryggvasons gate 1. I tillegg til herlig boble te i et stort utvalg av smaker selges det også asiatiske snacks, nudler og brus her. Tag noen som burde bli med deg hit i kommentarfeltet! 😉`,
+      content: `Introducing Our Refreshing Summer Sensation: Dive into the Cool Bliss of Bubble Tea! 🌞🥤
+
+        Embark on a journey of tantalizing taste with our newest summer creation at Boba Joy Trondheim! Escape the heat and indulge in our handcrafted, icy-cool bubble teas, perfectly crafted to quench your thirst and elevate your summer vibes.
+        
+        Made with the finest ingredients and bursting with flavor, our summer specials promise a delightful sip in every bubble. Whether you crave the fruity burst of mango, the creamy richness of coconut, or the exotic allure of lychee, we have a tantalizing flavor for every palate.
+        
+        Beat the heat and sip on sunshine with our limited-time summer menu. Visit us today and treat yourself to the ultimate summer refreshment!
+        
+        🌟 Try our new summer sensations today!
+        📍 Olav Tryggvasons gt. 1, 7011 Trondheim, Norway
+        📱 Follow us on Instagram and Facebook: @bobajoy.no / @bobajoytrondheim
+        #SummerSips #BubbleTeaBliss #RefreshingBeverages`,
     },
     {
       img: 'https://lh3.googleusercontent.com/geougc/AF1QipN7pDc7noCDrzj2sLnlxzc9Pzl_w69M52uRQ3lN=h305-no',
