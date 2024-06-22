3dcdf4136bbe60ea11ce06a0fc307ac69b22c5b5
bobajoytrondheim <bobajoytrondheim@gmail.com>
2024-06-22 13:08:24 +0700

    enter

--- pages/api/posts.ts
+++ pages/api/posts.ts
@@ -4,9 +4,10 @@ export default function handler(_req: any, res: any) {
     {
       img: 'https://lh3.googleusercontent.com/p/AF1QipP1X9w0PVeWzZ3iGtIcr0Pt8TT66rPQ82YNHAEE=s680-w680-h510',
       date: new Date('2022-03-01T11:30:32.601Z'),
-      content: '🎉🥤 Double the Tea, Double the Love! 🥤🎉
+      content: `🎉🥤 Double the Tea, Double the Love🥤🎉
 
-Share the love with our free tea giveaway! Ready to enjoy some bubble love with your bestie? We're giving away FREE Bubble Tea for you and a friend! 👫✨
+Share the love with our free tea giveaway! Ready to enjoy some bubble love with your bestie? 
+We're giving away FREE Bubble Tea for you and a friend! 👫✨
 
 Here's how to join:
 
@@ -15,7 +16,7 @@ Like this post ❤️
 Tag your bestie and comment a number between 11-99 👇
 Winners will be announced next Friday! Good luck! When you win, come to Boba Joy with your friend to receive your 2 cups of tea! 🍃✨
 
-#BobaJoy #ShareTheLove #BubbleTeaGiveaway',
+#BobaJoy #ShareTheLove #BubbleTeaGiveaway.`,
     },
     {
       img: 'https://lh3.googleusercontent.com/geougc/AF1QipN7pDc7noCDrzj2sLnlxzc9Pzl_w69M52uRQ3lN=h305-no',
@@ -31,7 +32,8 @@ Winners will be announced next Friday! Good luck! When you win, come to Boba Joy
       img: 'https://lh3.googleusercontent.com/geougc/AF1QipP0hd2N0qp2aSq3frIYVUfDTRD3bLvnzJgFYTG6=h305-no',
       date: new Date('2022-03-01T11:30:32.601Z'),
       content:
-        'Velkommen til Midtbyen, Boba Joy Trondheim !  Boble-te utvalget i Midtbyen vokser seg stadig større, og for kun to uker siden åpnet denne sjarmerende Kaféen i Olav Tryggvasons gate 1. I tillegg til herlig boble te i et stort utvalg av smaker selges det også asiatiske snacks, nudler og brus her. Tag noen som burde bli med deg hit i kommentarfeltet! 😉',
+        `Velkommen til Midtbyen, Boba Joy Trondheim ! 
+      Boble-te utvalget i Midtbyen vokser seg stadig større, og for kun to uker siden åpnet denne sjarmerende Kaféen i Olav Tryggvasons gate 1. I tillegg til herlig boble te i et stort utvalg av smaker selges det også asiatiske snacks, nudler og brus her. Tag noen som burde bli med deg hit i kommentarfeltet! 😉`,
     },
   ]);
 }
